e8ea1714873efd60ed1c2deb69f88fb3eee6c7e8
Anuradha <anuborpe04@gmail.com>
2021-09-16 00:31:45 +0530

    project1

--- src/main/java/com/whatsappweb/controller/deleteProfileServlet.java
+++ src/main/java/com/whatsappweb/controller/deleteProfileServlet.java
@@ -17,7 +17,7 @@ import com.whatsappweb.utility.ControllerFactory;
 
 public class deleteProfileServlet extends HttpServlet {
 	
-
+//deleteServlet
 	
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		HttpSession ss=request.getSession(true);
